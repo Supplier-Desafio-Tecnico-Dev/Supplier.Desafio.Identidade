1daf2063b125b07d07d44f0e9f67c9298336e76e
Igor Daflon do Couto <igor.couto@softplan.com.br>
2025-02-26 11:55:25 -0300

    Cria serviço de login para usuário

--- src/Supplier.Desafio.Identidade.Aplicacao/Usuarios/Servicos/UsuariosAppServico.cs
+++ src/Supplier.Desafio.Identidade.Aplicacao/Usuarios/Servicos/UsuariosAppServico.cs
@@ -4,6 +4,7 @@ using Supplier.Desafio.Identidade.Aplicacao.Usuarios.Servicos.Interfaces;
 using Supplier.Desafio.Identidade.DataTransfer.Usuarios.Requests;
 using Supplier.Desafio.Identidade.DataTransfer.Usuarios.Requests.Validadores;
 using Supplier.Desafio.Identidade.DataTransfer.Usuarios.Responses;
+using Supplier.Desafio.Identidade.Dominio.Core;
 using Supplier.Desafio.Identidade.Dominio.Usuarios.Entidades;
 
 namespace Supplier.Desafio.Identidade.Aplicacao.Usuarios.Servicos;
@@ -34,5 +35,26 @@ public class UsuariosAppServico : ServicoBase, IUsuariosAppServico
         // Persistir cliente
         
         return new UsuarioNovoResponse { Id = usuario.Id };
+    }
+    
+    public async Task<UsuarioAutenticarResponse> AutenticarAsync(UsuarioAutenticarRequest request)
+    {
+        if (!ExecutarValidacao(new UsuarioAutenticarRequestValidator(), request))
+            return new UsuarioAutenticarResponse { };
+        
+        // Buscar usuário no banco de dados
+        var usuario = await _usuarioRepository.ObterPorEmailAsync(request.Email);
+        if (usuario == null)
+            return new UsuarioAutenticarResponse { Sucesso = false, Mensagem = "Usuário ou senha inválidos." };
+
+        // Validar senha
+        var senha = CryptoHelper.Decrypt(usuario.Senha, "");
+        if (senha != request.Senha)
+            return new UsuarioAutenticarResponse { Sucesso = false, Mensagem = "Usuário ou senha inválidos." };
+
+        // Gerar token JWT
+        var token = _jwtService.GerarToken(usuario);
+        
+        return new UsuarioAutenticarResponse { Token = token };
     }
 }
